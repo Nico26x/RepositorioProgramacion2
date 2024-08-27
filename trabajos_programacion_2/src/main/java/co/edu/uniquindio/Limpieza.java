96f63a9b72fcc601704ac46868a45c6716e38348
Nico26x <nicolas.loaizab@uqvirtual.edu.co>
2024-08-26 23:37:00 -0500

    Nuevas clases que heredan de servicio

---
+++ trabajos_programacion_2/src/main/java/co/edu/uniquindio/Limpieza.java
@@ -0,0 +1,9 @@
+package co.edu.uniquindio;
+
+public class Limpieza extends Servicio {
+
+    public void consumible(){
+        
+    }
+    
+}
--- trabajos_programacion_2/src/main/java/co/edu/uniquindio/Servicio.java
+++ trabajos_programacion_2/src/main/java/co/edu/uniquindio/Servicio.java
@@ -1,27 +1,8 @@
 package co.edu.uniquindio;
 
-public class Servicio {
+public class Servicio implements Iconsumible {
 
-    private final String spa;
-    private final String restaurante;
-    private final String limpieza;
-    
-    public Servicio(String spa, String restaurante, String limpieza) {
-        this.spa = spa;
-        this.restaurante = restaurante;
-        this.limpieza = limpieza;
-    }
-
-    public String getSpa() {
-        return spa;
-    }
-
-    public String getRestaurante() {
-        return restaurante;
-    }
-
-    public String getLimpieza() {
-        return limpieza;
+    public void consumir(){
     }
 
 }
