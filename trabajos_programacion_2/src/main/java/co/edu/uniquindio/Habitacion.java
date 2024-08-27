5d8446a645109f1a0c04d4514dc3c5d28f3d1a3b
Nico26x <nicolas.loaizab@uqvirtual.edu.co>
2024-08-26 23:54:04 -0500

    Documentacion de las clases cliente, doble, simple, suit y habitacion

--- trabajos_programacion_2/src/main/java/co/edu/uniquindio/Habitacion.java
+++ trabajos_programacion_2/src/main/java/co/edu/uniquindio/Habitacion.java
@@ -1,19 +1,35 @@
 package co.edu.uniquindio;
 
-public  abstract class Habitacion {
+public abstract class Habitacion {
 
     private final int numero;
     private final double precio;
     
+    /**
+     * Método constructor de la clase abstracta Habitacion
+     * 
+     * @param numero, el numero de la habitacion
+     * @param precio, precio de la habitacion
+     */
     public Habitacion(int numero, double precio) {
         this.numero = numero;
         this.precio = precio;
     }
 
+     /**
+     * Método para obtener el valor del numero de la habitacion
+     * 
+     * @return valor del numero de la habitacion
+     */
     public int getNumero() {
         return numero;
     }
 
+     /**
+     * Método para obtener el valor del precio de la habitacion
+     * 
+     * @return valor del precio de la habitacion
+     */
     public double getPrecio() {
         return precio;
     }
