2f6a3b9577c1629f3e5d93805ae5da8e23c6dcbc
Nico26x <nicolas.loaizab@uqvirtual.edu.co>
2024-08-27 21:40:02 -0500

    Commit

--- trabajos_programacion_2/src/main/java/co/edu/uniquindio/Suite.java
+++ trabajos_programacion_2/src/main/java/co/edu/uniquindio/Suite.java
@@ -2,6 +2,10 @@ package co.edu.uniquindio;
 
 public class Suite extends Habitacion{
 
+    /**
+     * Método constructor de la clase Doble
+     * 
+     */
     public Suite(int numero, double precio){
         super(numero, precio);
 
